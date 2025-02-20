952a48c8ee8b485d99ecbb267aac3a50c5fefec0
mahalepranav <pranavmahale735@gmail.com>
2025-02-20 16:10:54 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const cors = require("cors");
+const connectDB = require("./config/db");
+const dotenv = require("dotenv");
+const router = require("./routes/index");
+const cookieParser = require("cookie-parser");
+
+dotenv.config();
+
+const app = express();
+
+// CORS configuration
+const corsOptions = {
+    // origin: "https://stationary-ecommerce-frontend.vercel.app",
+    origin: "*",  
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,  
+};
+
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(cookieParser());
+
+app.use("/api",router);
+
+const PORT = process.env.PORT || 8080
+
+app.get("/", (req, res) => {
+    res.send("hello from server");
+});
+
+connectDB();
+
+app.listen(PORT, () => {
+    console.log(`server is runnung on ${PORT}`);
+});
